1d2d7c38ef86b0f386e4d39974df83d00aab5937
DESKTOP-V4M5IG8\QuyLAPTOP <ledinhquy280799@gmail.com>
2021-10-20 14:11:36 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var count = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100 && count >= 100) {
+        clearInterval(id);
+        i = 0;
+        document.getElementById("loader-wrapper").style.display = "none";
+      } else {
+        width++;
+        count++;
+        document.getElementById("percent").innerHTML = count + "%";
+        elem.style.width = width + "%";
+      }
+    }
+  }
+
+}
+$(window).on("load", move());
+$('.owl-carousel').owlCarousel({
+    items:1,
+    lazyLoad:true,
+    loop:false,
+    margin:10
+});
